59c05b00d488ef68c4d9096a1120e4f4822cafa6
Anurag Singh <anuragsinghpatel17@gmail.com>
2024-12-27 08:55:47 +0530

    updated view

--- src/store/views.py
+++ src/store/views.py
@@ -3,8 +3,9 @@ from visits.models import PageVisit
 
 
 def home_page_view(request, *args, **kwargs):
-    queryset = PageVisit.objects.all()
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
     path = request.path
     PageVisit.objects.create(path=path)
-    context = {'page_title': 'Home', 'queryset': queryset}
+    context = {'page_title': 'Home', 'page_visit_count': page_qs.count(), 'total_visit_count':qs.count()}
     return render(request, "home.html", context)
