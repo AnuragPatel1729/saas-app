0aea73c3e9bebb39bcd5a43d717d77615e30fcef
Anurag Singh <anuragsinghpatel17@gmail.com>
2024-12-27 10:12:33 +0530

    using decouple

--- src/store/settings.py
+++ src/store/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ap^o*ew!^)b^c))a6rqr^#u*($0f_6vt!gupcez33s^*yq-ny'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+
+print("DEBGUG",DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app",
